efbe7b642d16cd9fb8f7a6616ee0d12842aeae77
William Weatherholtz <williamweatherholtz@gmail.com>
2023-09-13 14:33:36 -0400

    bad semantics fix - was returning early

--- src/backup_tool/main.py
+++ src/backup_tool/main.py
@@ -39,18 +39,27 @@ class FileList:
     pass in a directory, get a list of all files, optionally ONLY of a certain extension
     """
     dir: str
-    ext_filter: str = '.*'
+    ext_filters: str = '.*'
     
-    def search(self) -> dict:
+    def search(self) -> dict:        
         sizes = dict()
-        files = glob.glob(self.dir + f'/**/*{self.ext_filter}', recursive=True)
-        for file in files:
-            size = os.path.getsize(file)
-            
-            if size in sizes:
-                sizes[size].append(FileData(file))
-            else:
-                sizes[size] = [FileData(file)]
+        
+        for ext in self.ext_filters:
+            print (f'searching for {ext} files in {self.dir}...', end='')
+            files_found = 0
+            # not really handling upper & lower case, but we're currently using a windows system rn so...
+                
+            files = glob.iglob(self.dir + f'/**/*{ext}', recursive=True)
+            for file in files:
+                files_found += 1
+                size = os.path.getsize(file)
+                
+                if size in sizes:
+                    sizes[size].append(FileData(file))
+                else:
+                    #print (f'found unique size, making {len(sizes.values())} sizes total')
+                    sizes[size] = [FileData(file)]
+            print (f'found {files_found}')
         
         return sizes
     
@@ -93,34 +102,41 @@ class ListComparator:
         dest_sizes = self.dest_list.search()
         source_sizes = self.source_list.search()
         
+        matches_found = 0
+        copyme = []
+        
         for size, filedatas in source_sizes.items():
-            if size not in dest_sizes:
+            if size not in dest_sizes.keys():
                 # mutate source_list
                 for file in source_sizes[size]:
                     file.copyme = True
                 continue
             
-            # if size IS already in list, we need to compute checksums
+            # if size IS already in list, we need to compute checksums to know if they're the same or not
             dest_files = dest_sizes[size]
             source_files = source_sizes[size]
             
-            for fileset in [dest_files, source_files]:                
+            # calculate checksums for all files of this size, both in src and dest dirs
+            for fileset in [dest_files, source_files]:
                 for file in fileset:
-                    file.copyme = True
                     with open(file.filename, 'rb') as f:
-                        digest = hashlib.file_digest(f, 'blake2b')
+                        digest = hashlib.file_digest(f, 'blake2b') #blake2b is robust and fast, apparently
                     file.checksum = digest.hexdigest()
             
-            copyme = []
+            # now we can check to see if the source files are really unique
             for file in source_files:
                 dest_checksums = [f.checksum for f in dest_files]
                 #skip if already exists
                 
                 if file.checksum in dest_checksums:
+                    matches_found += 1
                     continue
                 
-                copyme.append(file)
-            return copyme
+                file.copyme = True
+            
+            copyme.extend(f for f in source_files if f.copyme)
+        print (f'found existing backups for {matches_found} files')
+        return copyme
         
 
 
@@ -131,27 +147,53 @@ if __name__ == '__main__':
     SOURCE_DIR = './src/backup_tool'
     DEST_DIR = './test_dest'
     
-        
-    fl = FileList(SOURCE_DIR, ext_filter='.bmp')
-    fl2 = FileList(DEST_DIR, ext_filter='.bmp')
+    SOURCE_DIR = 'Y:\\Pictures to backup\\unsorted_photos'
+    DEST_DIR = 'Y:\\Pictures'
+    #DEST_DIR = 'Y:\Pictures\kelly_single'
     
+    extensions = ['.jpg', '.jpeg', '.jpe', '.jig', '.jfif', '.jfi',
+                  '.png',
+                  '.gif',
+                  '.webp',
+                  '.tiff', '.tif',
+                  '.raw', '.arw', '.cr2', '.nrw', '.k25',
+                  '.heif', '.heic', 
+                  '.ind', '.indd', '.indt',
+                  '.svg', '.svgz',
+                  '.ai',
+                  '.eps',]
+                  #'.pdf',
+                  
+    extensions = ['.jpg', '.jpeg', 
+                  '.png',
+                  '.gif',
+                  '.svg',]
+                  
+    
+        
+    fl = FileList(SOURCE_DIR, ext_filters=extensions)
+    fl2 = FileList(DEST_DIR, ext_filters=extensions)
+    
+    '''
     t_fl = time.process_time()
     files = fl.search()
-    print(f'source dir search took {time.process_time() - t_fl} seconds')
+    print(f'source dir search took {time.process_time() - t_fl} minutes')
     t_fl2 = time.process_time()
     files2 = fl2.search()
-    print(f'dest dir search took {time.process_time() - t_fl2} seconds')
+    print(f'dest dir search took {time.process_time() - t_fl2} minutes')
     print()
+    '''
     
     c = ListComparator(fl, fl2)
     copylist = c.compare()
+    print (f'copy list is {len(copylist)} items long')
     ListComparator.save_files(copylist, SOURCE_DIR, DEST_DIR)
-    #print (f'copy list: {copylist}')
 
+    '''
     print()
     #print (files)
-    print (f'{len(fl)} files found in {SOURCE_DIR} with extension {fl.ext_filter}')
+    print (f'{len(fl)} files found in {SOURCE_DIR} with extension {fl.ext_filters}')
     #print (files)
-    print (f'{len(fl2)} files found in {DEST_DIR} with extension {fl2.ext_filter}')
+    print (f'{len(fl2)} files found in {DEST_DIR} with extension {fl2.ext_filters}')
     #print (files2)
     #'''
